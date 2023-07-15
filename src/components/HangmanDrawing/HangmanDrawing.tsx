3c15b1911b6fd0058a83d6d4c3ad855b677d1e65
Rupam Kerketta <38970183+rupamkerketta@users.noreply.github.com>
2023-07-15 12:07:29 +0530

    feat: add hang pole and body parts :sparkles:

--- src/components/HangmanDrawing/HangmanDrawing.tsx
+++ src/components/HangmanDrawing/HangmanDrawing.tsx
@@ -1,7 +1,26 @@
+// Styles
+import "./HangmanDrawing.scss";
+
+const HEAD = <div className="head"></div>;
+const BODY = <div className="body"></div>;
+const RIGHT_ARM = <div className="right-arm"></div>;
+const LEFT_ARM = <div className="left-arm"></div>;
+const RIGHT_LEG = <div className="right-leg"></div>;
+const LEFT_LEG = <div className="left-leg"></div>;
+
 export function HangmanDrawing() {
   return (
-    <div>
-      <h1>Hangman Drawing</h1>
+    <div className="hangman-drawing-container">
+      {HEAD}
+      {BODY}
+      {LEFT_ARM}
+      {LEFT_LEG}
+      {RIGHT_ARM}
+      {RIGHT_LEG}
+      <div className="hanging-tip"></div>
+      <div className="top"></div>
+      <div className="main"></div>
+      <div className="base"></div>
     </div>
   );
 }
